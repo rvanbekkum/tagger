fbf9c5c51f00ba5bf0bb45ccb19ff89952c23a7f
Aaron Ang <awz.ang@gmail.com>
2016-06-06 17:04:55 +0200

    Convert user tags to lists such that the labels can be binarized

---
+++ binarize_labels.py
@@ -0,0 +1,23 @@
+from sklearn.preprocessing import MultiLabelBinarizer
+import csv
+import re
+
+IMAGE_DATA = 'image_data.tsv'
+
+def tsv_to_list(csvinput):
+    labels = []
+    with open(csvinput) as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        for row in reader:
+            tags = row[10].split(',')
+            tags_filtered = filter_tags(tags)
+            labels.append(tags_filtered)
+    return labels
+
+def filter_tags(tags):
+    regex = '(\%[A-Z0-9]{2})+'
+    return [tag for tag in tags if not(re.search(regex, tag))]
+
+
+y = tsv_to_list(IMAGE_DATA)
+MultiLabelBinarizer().fit_transform(y)
