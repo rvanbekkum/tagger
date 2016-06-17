4a43123ff2a8d6aadebc87c26234e25a980c1e06
Aaron Ang <awz.ang@gmail.com>
2016-06-17 20:34:37 +0200

    Update training set extraction such that two seperate CSV files are created

--- tagger/scripts/create_training_set.py
+++ tagger/scripts/create_training_set.py
@@ -5,19 +5,32 @@ ORIGINAL_TRAINING_SIZE = 28157620
 NEW_TRAINING_SIZE = 10000
 TEST_SIZE = 2000
 INPUT = '../data/tag-train'
-OUTPUT = '../data/training_data.csv'
+OUTPUT_TRAIN = '../data/training_data.csv'
+OUTPUT_TEST = '../data/test_data.csv'
 
-image_positions = set()
+image_training_positions = set()
+image_test_positions = set()
 
-while len(image_positions) < NEW_TRAINING_SIZE + TEST_SIZE:
-    image_positions.add(randint(0, ORIGINAL_TRAINING_SIZE - 1))
+while len(image_training_positions) < NEW_TRAINING_SIZE:
+    image_training_positions.add(randint(0, ORIGINAL_TRAINING_SIZE - 1))
+
+while len(image_test_positions) < TEST_SIZE:
+    pos = randint(0, ORIGINAL_TRAINING_SIZE - 1)
+    if pos not in image_training_positions:
+        image_test_positions.add(pos)
 
 with open(INPUT, 'r') as csvinput:
     reader = csv.reader(csvinput, delimiter='\t')
-    with open(OUTPUT, 'w') as csvoutput:
-        writer = csv.writer(csvoutput)
-        for i, row in enumerate(reader):
-            if i in image_positions:
-                row.pop(0)
-                writer.writerow(row)
-                print(row)
+    with open(OUTPUT_TRAIN, 'w') as csv_train:
+        train_writer = csv.writer(csv_train)
+        with open(OUTPUT_TEST, 'w') as csv_test:
+            test_writer = csv.writer(csv_test)
+            for i, row in enumerate(reader):
+                if i in image_training_positions:
+                    row.pop(0)
+                    train_writer.writerow(row)
+                    print('TRAINING DATA: ', row)
+                if i in image_test_positions:
+                    row.pop(0)
+                    test_writer.writerow(row)
+                    print('TESTING DATA: ', row)
