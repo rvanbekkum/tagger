aa077707b7f513b1f5ae367ce351058cd01c3ed3
Aaron Ang <awz.ang@gmail.com>
2016-06-19 16:17:53 +0200

    Output predicted labels and true labels

--- tagger/benchmark.py
+++ tagger/benchmark.py
@@ -18,7 +18,15 @@ print('Performing k-fold cross-validation with k = 10...')
 
 kf = KFold(X.shape[0], n_folds=10)
 
+labels = np.load('data/labels/labels.npy')
+
 for train, test in kf:
     X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
     clf = OneVsRestClassifier(SVC(kernel='linear')).fit(X_train, y_train)
-    print(clf.score(X_test, y_test))
+    # print(clf.score(X_test, y_test))
+    prediction = clf.predict(X_test)
+    y_array = np.array(y_test).tolist()
+    for p, t in zip(prediction, y_array):
+        results = [labels[i] for i, x in enumerate(p) if x == 1]
+        results_t = [labels[i] for i, x in enumerate(t) if x == 1]
+        print(results, results_t)
